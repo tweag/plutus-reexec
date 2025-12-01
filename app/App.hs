07068ea147e28f61398166fc600d507997419885
Adithya Kumar <adithya.kumar@tweag.io>
2025-12-01 19:30:38 +0530

    Add the initial skeleton for streaming the chain

--- app/App.hs
+++ app/App.hs
@@ -1,4 +1,21 @@
-import Lib (helloWorld)
+--------------------------------------------------------------------------------
+-- Imports
+--------------------------------------------------------------------------------
+
+import Cardano.Api (ChainPoint(..))
+import Data.Function ((&))
+import PSR.Streaming (streamChainSyncEvents)
+import Streamly.Data.Stream.Prelude qualified as Stream
+import Streamly.Data.Fold.Prelude qualified as Fold
+
+--------------------------------------------------------------------------------
+-- Main
+--------------------------------------------------------------------------------
 
 main :: IO ()
-main = putStrLn helloWorld
+main = do
+  socketPath <- error "Get socket path from CLI"
+  networkId <- error "Get network id from CLI"
+  let points = [ChainPointAtGenesis]
+  streamChainSyncEvents socketPath networkId points
+    & Stream.fold (Fold.drainMapM print)

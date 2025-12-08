ea80da33238a801d2aa096ea0883f53be2f5bb29
Alex Mason <alex.mason@tweag.io>
2025-12-08 17:43:15 +1100

    Simplify main, print script details when found

--- app/App.hs
+++ app/App.hs
@@ -4,20 +4,19 @@
 
 import Cardano.Api qualified as C
 import Control.Concurrent.Async qualified as Async
+import Control.Monad (guard)
 import Data.Function ((&))
 import Data.Map qualified as Map
 import Data.Set qualified as Set
-import Data.Yaml (decodeFileThrow)
 import Options
 import Options.Applicative
-import qualified PSR.HTTP as HTTP 
+import qualified PSR.HTTP as HTTP
 import PSR.Chain
 import PSR.ConfigMap qualified as CM
 import PSR.ContextBuilder
 import PSR.Streaming
 import Streamly.Data.Fold.Prelude qualified as Fold
 import Streamly.Data.Stream.Prelude qualified as Stream
-import System.Exit (exitFailure)
 
 --------------------------------------------------------------------------------
 -- Main
@@ -26,23 +25,17 @@ import System.Exit (exitFailure)
 main :: IO ()
 main = do
     Options{..} <- execParser psrOpts
-    CM.ConfigMap{..} <- decodeFileThrow scriptYaml
-    -- TODO: Remove
-    -- TODO: Is there really no better way to parse a hash???
-    hash <- case C.deserialiseFromJSON "\"082efb838a6a38c435bf5a1c823569ad224fab42034d265b95f88665ee27877f\"" of
-        Left err -> do
-            print err
-            exitFailure
-        Right h -> pure h
-    let points = maybe [C.ChainPoint 173166134 hash] pure start -- [ChainPointAtGenesis]
+    CM.ConfigMap{..} <- either error pure =<< CM.readConfigMap scriptYaml
+
+    -- TODO: What's a better default here?
+    let points = maybe [C.ChainPointAtGenesis] pure cmStart
     -- TODO: Use a logging interface instead of using putStrLn.
     putStrLn "Started..."
 
     Async.withAsync (HTTP.run httpServerPort) $ \serverAsync -> do
       Async.link serverAsync
 
-      let confPolicyMap = Map.fromList [(CM.script_hash x, x) | x <- scripts]
-          conn = mkLocalNodeConnectInfo networkId socketPath
+      let conn = mkLocalNodeConnectInfo networkId socketPath
       streamChainSyncEvents conn points
           & Stream.filter (not . isByron)
           & fmap getEventTransactions
@@ -50,9 +43,13 @@ main = do
           -- TODO: Try to replace "concatMap" with "unfoldEach".
           & Stream.concatMap (Stream.fromList . (\(a, b) -> (a,) <$> b))
           & Stream.mapM (mkContext1 conn . uncurry mkContext0)
-          & Stream.filter
+          & Stream.mapMaybe
               ( \ctx1@Context1{..} ->
-                  not . Map.null . Map.restrictKeys confPolicyMap $
-                      Set.union (getMintPolicies context0) (getSpendPolicies ctx1)
+                  let interestingScripts =
+                          Map.restrictKeys cmScripts $
+                              Set.union (getMintPolicies context0) (getSpendPolicies ctx1)
+                   in do
+                          guard (not $ Map.null interestingScripts)
+                          pure (ctx1, interestingScripts)
               )
-          & Stream.fold (Fold.drainMapM print)
+          & Stream.fold (Fold.drainMapM (\(ctx, scripts) -> print ctx >> mapM_ print scripts))

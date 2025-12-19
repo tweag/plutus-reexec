b028aba1d234fb9705e2e8388d183dd6cddbdf9b
Alex Mason <alex.mason@tweag.io>
2025-12-19 19:41:48 +1100

    Simplify App monad logging

--- lib/PSR/Types.hs
+++ lib/PSR/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module PSR.Types (
@@ -6,7 +7,6 @@ module PSR.Types (
     module Export,
     AppConfig (..),
     HasConfigMap (..),
-    HasLoggerEnv (..),
     ChainSyncEvent (..),
     ChainSyncEventException (..),
     Block (..),
@@ -19,36 +19,38 @@ module PSR.Types (
 
 import Cardano.Api qualified as C
 import Control.Exception (Exception)
+import Control.Monad.Except (ExceptT (..), MonadError, runExceptT)
+import Control.Monad.IO.Unlift
 import Control.Monad.Reader (MonadReader (..), ReaderT (..))
-import Data.Aeson.Types (Pair)
-import Data.Text (Text)
-import Data.Time (getCurrentTime)
 import GHC.Generics (Generic)
 import Lens.Micro.Platform
-import Log (LogLevel, logMessageIO)
-import Log.Class (MonadLog (..))
+import Lens.Micro.Platform qualified as Export (view)
 
-import Control.Monad.IO.Unlift
-import Log.Logger (LoggerEnv (..))
 import PSR.ConfigMap
+import PSR.Logging.Interface (HasLogger (..), Logger)
 import PSR.Storage.Interface
 
-import Control.Monad.Except (ExceptT (..), MonadError, runExceptT)
-import Lens.Micro.Platform qualified as Export (view)
-import Log.Class as Export (MonadLog (..), logAttention, logAttention_, logInfo, logInfo_, logTrace, logTrace_)
+--------------------------------------------------------------------------------
+-- App context
+--------------------------------------------------------------------------------
 
 data AppConfig = AppConfig
-    { _confLoggerEnv :: LoggerEnv
+    { _confLogger :: Logger
     , _confConfigMap :: ConfigMap
     , _confStorage :: Storage
     }
 
 -- class HasAppConfig a where
 --      appConfig :: Lens a AppConfig,
---      confLoggerEnv :: Lens a ConfigMap
+--      confLogger :: Lens a ConfigMap
 --      ...
 makeClassy ''AppConfig
 instance HasConfigMap AppConfig where configMap = confConfigMap
+instance HasLogger AppConfig where getLogger = view confLogger
+
+--------------------------------------------------------------------------------
+-- App monad
+--------------------------------------------------------------------------------
 
 newtype App context err a
     = App {_runApp :: ExceptT err (ReaderT context IO) a}
@@ -63,29 +65,6 @@ newtype App context err a
 
 runApp :: context -> App context err a -> IO (Either err a)
 runApp conf = flip runReaderT conf . runExceptT . _runApp
-
--- TODO: Move into its own module, and use record instead of
-class HasLoggerEnv a where
-    loggerEnv :: Lens' a LoggerEnv
-    loggerEnvData :: Lens' a [Pair]
-    loggerEnvData = loggerEnv . lens leData (\lEnv new -> lEnv{leData = new})
-    loggerEnvDomain :: Lens' a [Text]
-    loggerEnvDomain = loggerEnv . lens leDomain (\lEnv new -> lEnv{leDomain = new})
-    loggerEnvMaxLogLevel :: Lens' a LogLevel
-    loggerEnvMaxLogLevel = loggerEnv . lens leMaxLogLevel (\lEnv new -> lEnv{leMaxLogLevel = new})
-
-instance HasLoggerEnv LoggerEnv where loggerEnv = id
-instance HasLoggerEnv AppConfig where loggerEnv = confLoggerEnv
-instance (HasLoggerEnv context) => MonadLog (App context err) where
-    getLoggerEnv = view loggerEnv
-
-    localData pairs = local (loggerEnvData %~ (++ pairs))
-    localDomain dom = local (loggerEnvDomain %~ (++ [dom]))
-    localMaxLogLevel level = local (loggerEnvMaxLogLevel .~ level)
-
-    logMessage level message data_ = App . ExceptT . fmap Right . ReaderT $ \context -> do
-        time <- getCurrentTime
-        logMessageIO (context ^. loggerEnv) time level message data_
 
 --------------------------------------------------------------------------------
 -- Types
